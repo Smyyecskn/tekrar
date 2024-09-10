95b9568c38d569023051a045faeaccfb4a1ce0ea
smyye <smyyeoztrk43@gmail.com>
2024-09-10 20:02:05 +0300

    main

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 
 const Main = () => {
-  const style = {
+  const parStyle = {
     color: "green",
     backgroundColor: "yellow",
     fontSize: "20px",
   };
+  const pStyle = {
+    color: "violet",
+    backgroundColor: "purple",
+    fontSize: "20px",
+    textAlign: "center",
+    margin: "50px",
+    width: "50%",
+  };
   return (
     <div>
       <h2>MAIN SECTION</h2>
-      <p style={style}>
+      <p style={parStyle}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aut,
         dicta quasi aliquid possimus, assumenda quo adipisci ut qui non numquam,
         nemo fugiat et impedit corporis blanditiis tempora. Iste, laudantium.
       </p>
       <h5>SUB SECTION</h5>
-      <p>
+      <p style={pStyle}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
         libero recusandae earum autem repudiandae sit eligendi dignissimos eius
         animi neque quibusdam voluptatibus laborum, expedita doloribus.
@@ -25,7 +33,20 @@ const Main = () => {
 };
 
 function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+  return (
+    <img
+      style={{
+        backgroundColor: "blue",
+        color: "red",
+        margin: "10px",
+        borderRadius: "5px",
+        height: "80px",
+        width: "100px",
+      }}
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
 }
 
 export function Gallery() {
